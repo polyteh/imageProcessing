3cf2b7e403cc582bb128ddb96cf599261557ea61
polyteh <44604978+polyteh@users.noreply.github.com>
2019-01-09 18:10:59 +0200

    Add files via upload
    without using in the hedaer file

--- sourse/myImageProcessing.h
+++ sourse/myImageProcessing.h
@@ -4,9 +4,9 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-using std::string;
-using namespace cv;
-using std::vector;
+//using std::string;
+//using namespace cv;
+//using std::vector;
 
 
 
@@ -14,32 +14,32 @@ class myImageProcessing
 {
 	public:
 	myImageProcessing();//конструктор с картинкой по умолчанию
-	void setImage(string & imageName);
-	Mat & getOriginalImage(){return originalImage;};//загрузка картинки
-	Mat & getGrayscaleImage();	//преобоазование в серый
-	Mat & getTresholdImage(int tresh); //преобразование в бинарный с параметром от слайдера
+	void setImage(std::string & imageName);
+	cv::Mat & getOriginalImage(){return originalImage;};//загрузка картинки
+	cv::Mat & getGrayscaleImage();	//преобоазование в серый
+	cv::Mat & getTresholdImage(int tresh); //преобразование в бинарный с параметром от слайдера
 	void findContour();//поиск контуров
-	Mat & getContourImage();//отрисовка контура
-	Mat & getHullImage();//отрисовка hull
+	cv::Mat & getContourImage();//отрисовка контура
+	cv::Mat & getHullImage();//отрисовка hull
 
 
 	
 	
 	
 	private:
-	string imageName="d:\\Education\\C++\\projects\\image_processing\\sourse\\data\\ellipse.png";
-	Mat originalImage;
-	Mat BGR2GRAY_image;
-	Mat adaptiveTresholdImage;
-	Mat contoursImage;
-	Mat hullImage;
+	std::string imageName="d:\\Education\\C++\\projects\\image_processing\\sourse\\data\\ellipse.png";
+	cv::Mat originalImage;
+	cv::Mat BGR2GRAY_image;
+	cv::Mat adaptiveTresholdImage;
+	cv::Mat contoursImage;
+	cv::Mat hullImage;
 	int const lowTreshold=100;
 	int gain; //сделал переменную, чтобы отслеживать перемещение слайдера
 	
-	Mat imageCanny; //готовим место для хранения обработанного бинарного изображения
+	cv::Mat imageCanny; //готовим место для хранения обработанного бинарного изображения
 	
-	vector< vector<Point> > imageContours; //массив векторов точек для хранения контуров (FindContour), как я понял это вектор из векторов объектов типа Point
-	vector<Vec4i> imageHierarchy; //пока с ним мало разобрался, на уровне простых картинок
+	std::vector< std::vector<cv::Point> > imageContours; //массив векторов точек для хранения контуров (FindContour), как я понял это вектор из векторов объектов типа Point
+	std::vector<cv::Vec4i> imageHierarchy; //пока с ним мало разобрался, на уровне простых картинок
 	
 
 
